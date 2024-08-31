8687a35e80ed032da217df43c6ecf05d6d19d367
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-08-31 13:32:23 +0600

    refactor: remove unnecessary code

--- src/modules/api/api.routes.js
+++ src/modules/api/api.routes.js
@@ -7,45 +7,11 @@
  */
 
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
 
 import authRoutes from './auth/auth.routes.js';
 import routesConstants from '../../constant/routes.constants.js';
 
 const router = express.Router();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-// Path to the JSDoc documentation
-const docsPath = path.join(
-    process.cwd(),
-    'src',
-    'modules',
-    'api',
-    'documentation',
-    'code'
-);
-
-// Read the Swagger JSON from the file system
-const swaggerDocument = JSON.parse(
-    // fs.readFileSync('./src/modules/api/documentation/api/swagger.json', 'utf8')
-    fs.readFileSync(
-        path.join(__dirname, 'documentation', 'api', 'swagger.json'),
-        'utf8'
-    )
-);
-
-// Serve JSDoc documentation on the /api/v1/code-docs route
-router.use('/documentation/code', express.static(docsPath));
-
-// API documentation route setup
-router.use(
-    '/documentation/api',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
-);
 
 // Application routes
 router.use(`/${routesConstants.auth.routes}`, authRoutes);
